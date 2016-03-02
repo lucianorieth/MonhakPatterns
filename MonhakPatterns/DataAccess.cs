d7d539959b16a173a1f25891864227e7fb578091
lucianorieth <lucianorieth@outlook.com>
2016-03-02 17:57:46 -0300

    New updates

--- MonhakPatterns/DataAccess.cs
+++ MonhakPatterns/DataAccess.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
+﻿using System.Collections.Generic;
+
 using System.Data;
 using System.Data.SqlClient;
+using System;
 
 namespace MonhakPatterns
 {
